73a9fa6faac6d1a39ff31e89a133c30283938975
fuchujian <vajava@126.com>
2016-01-01 14:26:31 +0800

    完善

--- ithinking-web-manager/src/main/java/org/ithinking/web/manager/Application.java
+++ ithinking-web-manager/src/main/java/org/ithinking/web/manager/Application.java
@@ -1,12 +1,20 @@
 package org.ithinking.web.manager;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@EnableAutoConfiguration
-public class Application {
+@SpringBootApplication 
+// @Configuration,@EnableAutoConfiguration,@ComponentScan, @EnableWebMvc(自动检查)
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
